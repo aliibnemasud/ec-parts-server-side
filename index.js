c985788b8e97fba0574d368461fc1fac8edfb561
aliibnemasud <mdmasudrony@gmail.com>
2022-05-25 15:14:30 +0600

    Making order api and load order api updated

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ async function run() {
     try{
         await client.connect();        
         const toolsCollection = client.db('ecparts').collection('tools');
+        const ordersCollection = client.db('ecparts').collection('order');
 
         // Load all tools
         app.get('/tools', async (req, res)=>{            
@@ -39,6 +40,22 @@ async function run() {
             const query = {_id: ObjectId(id)};
             const result = await toolsCollection.findOne(query);
             res.send(result)
+        })
+
+        // Order api - working
+        app.post('/orders', async(req, res)=>{
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);                     
+        })
+        
+        // Load Orders
+
+        app.get('/orders', async (req, res)=>{            
+            const query = {};
+            const cursor = ordersCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);           
         })
 
 
