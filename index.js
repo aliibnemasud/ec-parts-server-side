58ed998430f3226d2e81f8a9cf020c3fcfd096ef
aliibnemasud <mdmasudrony@gmail.com>
2022-05-26 20:03:11 +0600

    All products showing added

--- index.js
+++ index.js
@@ -21,78 +21,105 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ecparts
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    try{
-        await client.connect();        
+    try {
+        await client.connect();
         const toolsCollection = client.db('ecparts').collection('tools');
         const ordersCollection = client.db('ecparts').collection('order');
         const usersCollection = client.db('ecparts').collection('users');
 
         // Load all tools
-        app.get('/tools', async (req, res)=>{            
+        app.get('/tools', async (req, res) => {
             const query = {};
             const cursor = toolsCollection.find(query);
             const tools = await cursor.toArray();
-            res.send(tools);           
+            res.send(tools);
+        })
+
+        // add new tools/product
+
+        app.post('/tools', async (req, res) => {
+            const tool = req.body;            
+            const result = await toolsCollection.insertOne(tool);                    
+            res.send(result);
         })
 
         // Load dynamic route for tools
-        app.get('/tools/:id', async (req, res)=>{
+        app.get('/tools/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await toolsCollection.findOne(query);
             res.send(result)
         })
 
         // Order api - working
-        app.post('/orders', async(req, res)=>{
+        app.post('/orders', async (req, res) => {
             const order = req.body;
             const result = await ordersCollection.insertOne(order);
-            res.send(result);                     
+            res.send(result);
         })
-        
+
         // Load Orders - individual
-        app.get('/orders', async (req, res)=>{            
+        app.get('/orders', async (req, res) => {
             const email = req.query.email;
-            const query = {email: email };
+            const query = { email: email };
             const cursor = ordersCollection.find(query);
             const orders = await cursor.toArray();
-            res.send(orders);           
+            res.send(orders);
         })
 
         // Delete Data
 
-        app.delete('/orders/:id', async (req, res)=>{ 
-            const id = req.params.id;           
-            const query = {_id: ObjectId(id)};
-            const result = await ordersCollection.deleteOne(query);            
-            res.send(result);           
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.send(result);
         })
 
         // register user to the database
-        app.put('/users/:email', async (req, res)=>{
+        app.put('/users/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
-            const filter = {email: email};
-            const option = {upsert: true};
+            const filter = { email: email };
+            const option = { upsert: true };
             const updateUser = {
                 $set: user,
             };
             const result = await usersCollection.updateOne(filter, updateUser, option);
-            res.send(result); 
+            res.send(result);
         })
 
         // Load Users
-        app.get('/users', async (req, res)=>{            
+        app.get('/users', async (req, res) => {
             const query = {};
             const cursor = usersCollection.find(query);
             const users = await cursor.toArray();
             res.send(users);
         })
+
+
+        // Make Admin
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateUser = {
+                $set: {role: 'admin'},
+            };
+            const result = await usersCollection.updateOne(filter, updateUser);
+            res.send(result);
+        })
+
+        // Add new product
+
+
+        // Last bracket of try
     }
+
+
 
     finally {
         // close
-    }    
+    }
 }
 
 run().catch(console.dir);
